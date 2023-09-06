658b5e3c18e6e605fe96732cb933ca542e8e6af3
MiiKiyoshi <kongkijung@gmail.com>
2023-09-06 16:29:00 +0900

    윈도우 lunarvim 쓰레X네 아오

--- lemmings_1.v
+++ lemmings_1.v
@@ -1,1 +1,24 @@
-sex
+module top_module(
+    input clk,
+    input areset,    // Freshly brainwashed Lemmings walk left.
+    input bump_left,
+    input bump_right,
+    output walk_left,
+    output walk_right); //  
+
+    // parameter LEFT=0, RIGHT=1, ...
+    reg state, next_state;
+
+    always @(*) begin
+        // State transition logic
+    end
+
+    always @(posedge clk, posedge areset) begin
+        // State flip-flops with asynchronous reset
+    end
+
+    // Output logic
+    // assign walk_left = (state == ...);
+    // assign walk_right = (state == ...);
+
+endmodule
